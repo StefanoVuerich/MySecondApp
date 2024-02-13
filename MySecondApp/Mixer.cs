8229033d0e495988e2ccbe533e74ec8720b24a56
Stefano Vuerich <vuerich@irsweb.it>
2024-02-13 10:47:23 +0100

    First commit

---
+++ MySecondApp/Mixer.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Mixer
+{
+    public List<Channel> InputChannels { get; set; }
+    public List<Channel> OutputChannels { get; set; }
+    private int volume;
+
+    //costruttore senza parametri
+    public Mixer()
+    {
+        InputChannels = new List<Channel>();
+        OutputChannels = new List<Channel>();
+        volume = 0;
+    }
+
+    //metodo che setta il volume con dei controlli
+    public void SetVolume(int newVolume)
+    {
+        if (newVolume > 100)
+        {
+            volume = 100;
+        }
+        else if (newVolume < 0)
+        {
+            volume = 0;
+        }
+        else
+        {
+            volume = newVolume;
+        }
+    }
+}
---
+++ MySecondApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var myMixer = new Mixer();
+Console.WriteLine("Imposta il volume");
+myMixer.SetVolume(Convert.ToInt32(Console.ReadLine()));
+
+
